b897e39e55541d43d526539f7e879738dfc011e1
JH <jh@psu.edu>
2020-10-09 17:16:05 -0400

    debugging like an idiot

--- run_simulations.py
+++ run_simulations.py
@@ -23,13 +23,13 @@ def run_simulation(args):
 
 
 parser = argparse.ArgumentParser(description="Run simulations for (private) spectral clustering")
-parser.add_argument("--nodes", type=int, description="Number of nodes in each simulated network")
-parser.add_argument("--blocks", type=int, description="Number of communities in each network")
-parser.add_argument("--r", type=float, description="Probability of edge across different blocks")
-parser.add_argument("--p", type=float, description="Additional probability of edge within block (p + r)")
-parser.add_argument("--runs", type=int, description="Number of simulations to run")
-parser.add_argument("--no-privacy", action='store_true', description="If specified, no privacy will be used")
-parser.add_argument("--epsilon", type=float, description="Privacy budget (>0)")
+parser.add_argument("--nodes", type=int, help="Number of nodes in each simulated network")
+parser.add_argument("--blocks", type=int, help="Number of communities in each network")
+parser.add_argument("--r", type=float, help="Probability of edge across different blocks")
+parser.add_argument("--p", type=float, help="Additional probability of edge within block (p + r)")
+parser.add_argument("--runs", type=int, help="Number of simulations to run")
+parser.add_argument("--no-privacy", action='store_true', help="If specified, no privacy will be used")
+parser.add_argument("--epsilon", type=float, help="Privacy budget (>0)")
 
 args = parser.parse_args()
 
